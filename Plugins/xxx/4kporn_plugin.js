7cd3cc9718496c49be792da23a0d0b748b21223d
alokillgtv-gif <alokillgtv@gmail.com>
2026-07-30 19:15:38 +0700

    1

--- Plugins/xxx/4kporn_plugin.js
+++ Plugins/xxx/4kporn_plugin.js
@@ -5,7 +5,7 @@ function getManifest() {
         "id": "4kporn",
         "name": "Phim XXX 4K",
         "description": "XXX siêu nét.",
-        "version": "1.5.7",
+        "version": "1.5.8",
         "baseUrl": "https://www.freepornvideos.xxx",
         "iconUrl": "https://raw.githubusercontent.com/alokillgtv-gif/VAXAPPSCRIPT/main/img/cnporn.jpg",
         "info":"Nguồn phim chất lượng 4K nên load hơi lâu, bạn chịu khó đợi tí nha.",
@@ -405,7 +405,6 @@ function parseDetailResponse(html, url) {
                 if (type.indexOf("2160") > -1) {
                     stream = link;
                 }
-                epi.push({ id: link + "#video.m3u8", name: "Độ Phân Giải " + type, slug: lname ? lname.replace(/\s/g, "_") : "" });
             }
         }
         log("Đang phát: " + url)
