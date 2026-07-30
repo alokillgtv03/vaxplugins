9ea8c75b442e27305b6256bd919af4427d2bcc76
alokillgtv-gif <alokillgtv@gmail.com>
2026-07-30 22:19:30 +0700

    1

--- Plugins/xxx/krx18_plugin.js
+++ Plugins/xxx/krx18_plugin.js
@@ -4,13 +4,13 @@ function getManifest() {
     return JSON.stringify({
         "id": "krx18",
         "name": "Phim 18+ Hàn",
-        "description": "Nguồn XXX hàn quốc Hay",
-        
-        "version": "1.1.3",
+        "info": "Nguồn phim Hàn Quốc siêu hay.\n\nNếu bị chặn các bạn hãy dùng App 1.1.1.1 hoặc thử bật DNS và DPI ở mục cài đặt APP nha."",
+        "version": "1.1.4",
         "BASEURL": "https://krx18.com",
         "iconUrl": "https://krx18.com/wp-content/uploads/2022/10/krx18B.png",
         "isEnabled": true,
         "isAdult": true,
+        "layoutType": "HORIZONTAL",
         "type": "VIDEO",
         "playerType": "embed"
     });
