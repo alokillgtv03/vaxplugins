ca2958d4e01ad1f29cf6056cc6d4dda0788b3351
alokillgtv-gif <alokillgtv@gmail.com>
2026-07-30 19:17:55 +0700

    1

--- Plugins/xxx/4kporn_plugin.js
+++ Plugins/xxx/4kporn_plugin.js
@@ -5,7 +5,7 @@ function getManifest() {
         "id": "4kporn",
         "name": "Phim XXX 4K",
         "description": "XXX siêu nét.",
-        "version": "1.5.8",
+        "version": "1.5.9",
         "baseUrl": "https://www.freepornvideos.xxx",
         "iconUrl": "https://raw.githubusercontent.com/alokillgtv-gif/VAXAPPSCRIPT/main/img/cnporn.jpg",
         "info":"Nguồn phim chất lượng 4K nên load hơi lâu, bạn chịu khó đợi tí nha.",
