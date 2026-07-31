08cccaf2a0ffcfb3d7cc606b72ba9dbaf380660a
alokillgtv-gif <alokillgtv@gmail.com>
2026-07-31 14:40:31 +0700

    1

--- Plugins/movie/vsmov_plugin.js
+++ Plugins/movie/vsmov_plugin.js
@@ -6,7 +6,7 @@ function getManifest() {
     id: "vsmov",
     name: "Nguồn Vsmov",
     description: "Nguồn phim Vsmov.",
-    "version": "1.0",
+    "version": "1.1",
     info: "Nguồn phim vietsub và thuyết minh mới.\n\n Hỗ trợ lồng tiếng và có tốc độ phát rất nhanh.",
     baseUrl: "https://vsmov.com",
     iconUrl: "https://vsmov.com/favicon-vsm.png",
@@ -112,7 +112,13 @@ function getUrlList(slug, filtersJson) {
         }
 
         // 3. Ghép URL an toàn với BASEURL
-        var resultUrl = BASEAPI;
+        if(path.indexOf("/danh-sach/phim-le") > -1 || path.indexOf("/danh-sach/phim-bo") > -1 || path.indexOf("/danh-sach/thuyet-minh") > -1 || path.indexOf("/danh-sach/long-tieng") > -1){
+            var resultUrl = BASEURL;
+        }
+        else{
+            var resultUrl = BASEAPI;
+        }
+        
         if (path) {
             resultUrl += (path.indexOf("/") === 0 ? "" : "/") + path;
         }
