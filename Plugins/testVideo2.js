43e35fad5b15ace721c0243ce88c6f088da6b9b7
alokillgtv-gif <alokillgtv@gmail.com>
2026-07-31 08:19:06 +0700

    1

--- Plugins/testVideo2.js
+++ Plugins/testVideo2.js
@@ -4,20 +4,22 @@
 // =============================================================================
 BaseURL = "https://script.google.com/macros/s/AKfycbydwasfO9sUsP7nSduOON6yKVZUMpSraNRFb58knwl_AKpb6vixCuPe-uptcpaGIiXBEw/exec";
 BaseJSON = "";
+BaseJSON2 = "";
 
 function getManifest() {
     return JSON.stringify({
-        "id": "testvideo3",          
-        "name": "Test EMBED TO Exoplayer",
+        "id": "testvideo2",          
+        "name": "Test Exoplayer",
         "description": "Nguồn xem phim Online ổn định",
-        "version": "1.5.4",             
-        "baseUrl": "https://script.google.com/macros/s/AKfycbydwasfO9sUsP7nSduOON6yKVZUMpSraNRFb58knwl_AKpb6vixCuPe-uptcpaGIiXBEw/exec",
+        "version": "2.5",             
+        "baseUrl": BaseURL,
         "iconUrl": "https://crimescenesolutions.co.za/wp-content/uploads/2026/04/phimhayok-io-fav.jpg", 
         "isEnabled": true,
-        "type": "MOVIE",
-        "playerType": "exoplayer"
+        "type": "VIDEO",
+        "playerType": "embed"
     });
 }
+
 
 function getHomeSections() {
     return JSON.stringify([
@@ -73,11 +75,15 @@ function getUrlYears() { return ""; }
 
 function parseListResponse(html) {
     try {
+        
         // Lưu trữ object đầu tiên trực tiếp vào BaseJSON toàn cục để các hàm sau dùng tiện lợi
+        var parsed = JSON.parse(html);
+        BaseJSON = Array.isArray(parsed) ? parsed[0] : parsed;
+        var $url = BaseJSON.url || "";
         var items = [];
         items.push({
             "id": BaseURL,          
-            "title": "Test", 
+            "title": $url, 
             "posterUrl": "https://img-cdn.phimhayok.net/filmhayok/1782912263995/20260701/ChatGPT-Image-19_29_49-1-thg-7-2026_a20d108246f140ad8be82acb9bca2606.png",  
             "backdropUrl": "https://img-cdn.phimhayok.net/filmhayok/1782912263995/20260701/ChatGPT-Image-19_29_49-1-thg-7-2026_a20d108246f140ad8be82acb9bca2606.png"
         });
@@ -96,64 +102,60 @@ function parseSearchResponse(html) {
 }
 
 function parseMovieDetail(html) {
-    try {
-        var id = BaseURL;
-        var parsed = JSON.parse(html);
-        BaseJSON = Array.isArray(parsed) ? parsed[0] : parsed;
-        var videoUrl = BaseJSON.link;
-        var $url = BaseJSON.url || "";
-        // Khai báo trước streamUrl chống lỗi Strict Mode khi eval thực thi
-        var streamUrl = ""; 
-        var title = "Chưa rõ tên phim";
-        var year = "2026";
-        var des = html;
-        var img = "https://img-cdn.phimhayok.net/filmhayok/1782912263995/20260701/ChatGPT-Image-19_29_49-1-thg-7-2026_a20d108246f140ad8be82acb9bca2606.png";
-        var episodes = [{ id: id, name: "Xem Ngay", slug: "full" }];
-        
-        return JSON.stringify({
-            "id": id,
-            "title": title,
-            "posterUrl": img,
-            "backdropUrl": img,
-            "description": des,
-            "year": year,
-            "rating": 10,
-            "quality": "HD",
-            "servers": [{ "name": "Server Vietsub", "episodes": episodes }]
-        });
-
-    } catch (e) {
-        return JSON.stringify({ "id": "error", "title": "Lỗi tải dữ liệu", "servers": [] });
-    }
+	try {
+		var id = BaseURL;
+		// Khai báo trước streamUrl chống lỗi Strict Mode khi eval thực thi
+		var streamUrl = "";
+		var rmatch = html.match(/id="streaming-sv"[^>]*?data-link="(https?:[^"]*)"/i);
+		if (rmatch && rmatch[1]) { streamUrl = rmatch[1]; }
+		var title = "Chưa rõ tên phim";
+		var year = "2026";
+		var des = streamUrl + "\r\n\r\n" + html;
+		var img = "https://img-cdn.phimhayok.net/filmhayok/1782912263995/20260701/ChatGPT-Image-19_29_49-1-thg-7-2026_a20d108246f140ad8be82acb9bca2606.png";
+		var episodes = [{ id: id, name: "Xem Ngay", slug: "full" }];
+		
+		return JSON.stringify({
+			"id": id,
+			"title": title,
+			"posterUrl": img,
+			"backdropUrl": img,
+			"description": des,
+			"year": year,
+			"rating": 10,
+			"quality": "HD",
+			"servers": [{ "name": "Server Vietsub", "episodes": episodes }]
+		});
+		
+	} catch (e) {
+		return JSON.stringify({ "id": "error", "title": "Lỗi tải dữ liệu", "servers": [] });
+	}
 }
 
-
-function parseDetailResponse(html,url) {
-    try {
-        // Đọc trực tiếp từ thuộc tính của BaseJSON đã lưu ở bước đầu tiên
-
-        var parsed = JSON.parse(html);
-        BaseJSON = Array.isArray(parsed) ? parsed[0] : parsed;
-        var videoUrl = BaseJSON.link || "";
-        var refUrl = BaseJSON.ref || "";
-        var agent = BaseJSON.codeb || "Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36";
-        var $type = BaseJSON.codea;
-		return JSON.stringify({
+function parseDetailResponse(html, url) {
+	try {
+		// Đọc trực tiếp từ thuộc tính của BaseJSON đã lưu ở bước đầu tiên
+		var parsed = JSON.parse(html);
+		BaseJSON = Array.isArray(parsed) ? parsed[0] : parsed;
+		var videoUrl = BaseJSON.link || "";
+		var refUrl = BaseJSON.ref || "";
+		var agent = BaseJSON.codeb || "Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36";
+		var customjs = BaseJSON.codec || "";
+        var type =     BaseJSON.codea;
+        return JSON.stringify({
             "url": videoUrl,
             "isEmbed": false,
-            "mimeType": $type,
+            "mimeType": "application/x-mpegURL",
             "headers": {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
                 "Referer": refUrl,
-
-        }
-    });
-      
-    } catch (e) {
-        return JSON.stringify({ "url": "", "headers": {} });
-    }
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+            },
+            "subtitles": []
+        });
+		
+	} catch (e) {
+		return JSON.stringify({ "url": "", "headers": {} });
+	}
 }
-
 
 function parseCategoriesResponse(html) { return "[]"; }
 function parseCountriesResponse(html) { return "[]"; }
