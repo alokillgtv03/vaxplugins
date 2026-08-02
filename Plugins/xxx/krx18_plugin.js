58ba3bae70d97154919eb5d56eec1517289a0331
alokillgtv03 <alokillgtv03@gmail.com>
2026-08-03 00:50:14 +0700

    update

--- Plugins/xxx/krx18_plugin.js
+++ Plugins/xxx/krx18_plugin.js
@@ -5,7 +5,7 @@ function getManifest() {
         "id": "krx18",
         "name": "Phim 18+ Hàn",
         "description": "Nguồn XXX hàn quốc Hay", 
-        "version": "1.1.9",
+        "version": "1.2.1",
         "BASEURL": "https://krx18.com",
         "iconUrl": "https://krx18.com/wp-content/uploads/2022/10/krx18B.png",
         "isEnabled": true,
